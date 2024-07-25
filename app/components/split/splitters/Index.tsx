bc5c9d26f9e1996d4ac4b9d2e4477a2e018a3acc
Bobotato <bhojwanialexander@gmail.com>
2024-07-26 02:41:59 +0800

    feat: Add alert to custom hook

--- app/components/split/splitters/Index.tsx
+++ app/components/split/splitters/Index.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import { useState } from "react";
+import useAlert from "@/app/hooks/useAlert";
 
-import Alert from "@mui/material/Alert";
-import AlertTitle from "@mui/material/AlertTitle";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import Snackbar from "@mui/material/Snackbar";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
 
 import AddSplitterForm from "@/app/components/split/splitters/AddSplitterForm";
 import SplitterList from "@/app/components/split/splitters/SplitterList";
@@ -19,53 +15,31 @@ import { isNameUsed } from "@/app/utils/split";
 import type { Splitter } from "@/app/types/UserTypes";
 
 export default function SplitterIndex() {
-  const [snackbarOpen, setSnackbarOpen] = useState<boolean>(false);
   const [splitterList, setSplitterList] = useState<Splitter[]>([]);
-
-  function AlertSnackbar() {
-    return (
-      <Snackbar
-        open={snackbarOpen}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-        autoHideDuration={5000}
-      >
-        <Alert
-          severity="success"
-          variant="filled"
-          sx={{ width: "100%" }}
-          action={
-            <IconButton
-              aria-label="close"
-              color="inherit"
-              size="small"
-              onClick={() => {
-                setSnackbarOpen(false);
-              }}
-            >
-              <CloseIcon fontSize="inherit" />
-            </IconButton>
-          }
-        >
-          <AlertTitle>Successfully Added</AlertTitle>
-          This is a success Alert inside a Snackbar!
-        </Alert>
-      </Snackbar>
-    );
-  }
+  const { AlertComponent, showAlert } = useAlert();
 
   function handleAddSplitter(splitter: Splitter) {
     try {
       isNameUsed(splitterList, splitter.name);
       setSplitterList((prev) => [...prev, splitter]);
-      setSnackbarOpen(true);
+      showAlert(
+        "Successfully Added!",
+        `${splitter.name} was added to the group.`,
+        "success"
+      );
     } catch (e) {
       console.error(e);
+      showAlert(
+        "Failed to add new splitter!",
+        `${splitter.name} was not added to the group.`,
+        "error"
+      );
     }
   }
 
   return (
     <Box>
-      <AlertSnackbar></AlertSnackbar>
+      <AlertComponent></AlertComponent>
       <Stack>
         <Typography>
           Your group currently consists of{" "}
---
+++ app/hooks/useAlert.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState } from "react";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Snackbar from "@mui/material/Snackbar";
+
+import type { AlertColor, AlertPropsColorOverrides } from "@mui/material/Alert";
+import type { OverridableStringUnion } from "@mui/types";
+
+export default function useAlert() {
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const [severity, setSeverity] =
+    useState<OverridableStringUnion<AlertColor, AlertPropsColorOverrides>>(
+      "success"
+    );
+
+  function showAlert(title: string, message: string, severity: any) {
+    setMessage(message);
+    setTitle(title);
+    setSeverity(severity);
+    setAlertOpen(true);
+  }
+
+  function closeAlert() {
+    setAlertOpen(false);
+  }
+
+  function AlertComponent() {
+    return (
+      <Snackbar
+        open={alertOpen}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        autoHideDuration={5000}
+        sx={{ p: 4, width: "100%", maxWidth: "md" }}
+      >
+        <Alert
+          severity={severity}
+          variant="filled"
+          sx={{ width: "100%" }}
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                closeAlert();
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+        >
+          <AlertTitle>{title}</AlertTitle>
+          {message}
+        </Alert>
+      </Snackbar>
+    );
+  }
+
+  return { showAlert, AlertComponent };
+}
