16abd4e3e6c150d0b07498b2fc4c739c00d6c3b6
Bobotato <bhojwanialexander@gmail.com>
2024-08-15 18:32:07 +0800

    feat: Add validation for login form

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -1,29 +1,43 @@
 "use client";
 
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
+import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import CircularProgress from "@mui/material/CircularProgress";
 
+import { useState } from "react";
 import { useRouter } from "next/navigation";
-import {
-  FormContainer,
-  TextFieldElement,
-  PasswordElement,
-} from "react-hook-form-mui";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { LoginSchema } from "@/schemas/forms/auth";
+
 import { Credentials } from "@/types/AuthTypes";
 
 export default function Login() {
-  const defaultValues: Credentials = {
-    email: "",
-    password: "",
-  };
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchema>({
+    mode: "all",
+    resolver: zodResolver(LoginSchema),
+  });
 
   const router = useRouter();
 
   async function handleLogin(loginCredentials: Credentials) {
     try {
+      setIsLoading(true);
       const response = await fetch("/api/auth/login", {
         method: "POST",
         body: JSON.stringify({
@@ -36,7 +50,13 @@ export default function Login() {
       }
     } catch (e) {
       console.error(e);
+    } finally {
+      setIsLoading(false);
     }
+  }
+
+  function handleShowPassword() {
+    setShowPassword((prev) => !prev);
   }
 
   return (
@@ -47,36 +67,54 @@ export default function Login() {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        gap: 4,
       }}
     >
-      <FormContainer defaultValues={defaultValues} onSuccess={handleLogin}>
-        <TextFieldElement
-          name={"email"}
-          label={"Email Address"}
-          autoComplete="email"
-          autoFocus
-          required
+      <form onSubmit={handleSubmit(handleLogin)} className="w-full">
+        <Stack direction="column" spacing={2}>
+          <TextField
+            {...register("email")}
+            label="Email *"
+            error={!!errors.email}
+            helperText={errors.email?.message}
+            fullWidth
+          ></TextField>
+          <TextField
+            {...register("password")}
+            label="Password *"
+            type={showPassword ? "text" : "password"}
+            error={!!errors.password}
+            helperText={errors.password?.message}
+            fullWidth
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleShowPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          ></TextField>
+
+          <Link variant="body1" href="/split">
+            Forgot password?
+          </Link>
+        </Stack>
+
+        <Button
+          type="submit"
           fullWidth
-          sx={{ mb: 2 }}
-        />
-
-        <PasswordElement
-          name={"password"}
-          label={"Password"}
-          required
-          fullWidth
-          sx={{ mb: 2 }}
-        />
-
-        <Link variant="body1" href="/split">
-          Forgot password?
-        </Link>
-
-        <Button type="submit" fullWidth variant="contained" sx={{ mt: 4 }}>
-          Log In
+          variant="contained"
+          sx={{ mt: 4 }}
+          disabled={isLoading}
+        >
+          {isLoading ? <CircularProgress size={25} /> : "Log In"}
         </Button>
-      </FormContainer>
+      </form>
     </Container>
   );
 }
--- components/split/AppBar.tsx
+++ components/split/AppBar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
@@ -8,10 +10,26 @@ import Stack from "@mui/material/Stack";
 import Image from "next/image";
 
 import type { ReactElement } from "react";
+import { useRouter } from "next/navigation";
 
 export default function ButtonAppBar(): ReactElement {
+  const router = useRouter();
+
+  async function handleLogout() {
+    try {
+      const response = await fetch("/api/auth/logout", {
+        method: "POST",
+      });
+      if (response.ok) {
+        router.push("/logout");
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
-    <Box sx={{ width: '100%', flexGrow: 1 }}>
+    <Box sx={{ width: "100%", flexGrow: 1 }}>
       <AppBar position="static" sx={{ p: 2 }}>
         <Toolbar
           sx={{ display: "flex", flexGrow: 1, justifyContent: "space-between" }}
@@ -26,7 +44,7 @@ export default function ButtonAppBar(): ReactElement {
           </Link>
 
           <Stack direction="row">
-            <Button href="/login" color="inherit">
+            <Button onClick={handleLogout} color="inherit">
               Logout
             </Button>
           </Stack>
