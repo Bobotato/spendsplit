1e28f06656c164cd6d71de1a3ed2b1fb577ff4b3
Bobotato <bhojwanialexander@gmail.com>
2024-08-29 04:03:03 +0800

    feat: Add delete group route

---
+++ app/api/split/groups/deleteGroupById/route.ts
@@ -0,0 +1,60 @@
+import { NextResponse, NextRequest } from "next/server";
+
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
+import { deleteGroupByGroupId } from "@/app/api/services/groups/groups";
+import { GroupNotFoundError } from "@/app/api/services/errors";
+
+async function POST(request: NextRequest) {
+    try {
+      const req = await request.json();
+      if (!req.groupId) {
+        return NextResponse.json(
+          { error: "Group id was not supplied." },
+          { status: 400 }
+        );
+      }
+      const res = await deleteGroupByGroupId(
+        req.groupId,
+      );
+  
+      return NextResponse.json(
+        { message: `Successfully deleted group ${req.groupId}.` },
+        { status: 200 }
+      );
+    } catch (e) {
+        console.log(e)
+      if (e instanceof SyntaxError) {
+        return NextResponse.json(
+          {
+            error: "There was no JSON body supplied.",
+          },
+          { status: 400 }
+        );
+      }
+      if (e instanceof PrismaClientKnownRequestError) {
+        return NextResponse.json(
+          {
+            error: "There was no group with this id found.",
+          },
+          { status: 404 }
+        );
+      }
+      if (e instanceof GroupNotFoundError) {
+        return NextResponse.json(
+          {
+            error: "There was no group with this id found.",
+          },
+          { status: 404 }
+        );
+      }
+      return NextResponse.json(
+        {
+          error: "There was an error deleting the group, please try again later.",
+        },
+        { status: 500 }
+      );
+    }
+  }
+  
+  export { POST };
