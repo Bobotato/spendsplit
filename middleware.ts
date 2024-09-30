d20bf4c77b8673571a2fb5387aa018a66de22504
Bobotato <bhojwanialexander@gmail.com>
2024-10-01 01:37:56 +0800

    feat: Update middleware to protect api routes

--- middleware.ts
+++ middleware.ts
@@ -4,26 +4,36 @@ import { verifyJWT } from "@/services/auth/auth";
 import { UnauthorisedError } from "@/services/errors";
 
 export const config = {
-  matcher: ["/home", "/groups/:path*"],
+  matcher: [
+    "/home",
+    "/groups/:path*",
+    "/api/admin/:path*",
+    "/api/split/:path*",
+  ],
 };
 
 async function middleware(request: NextRequest) {
   const token = request.cookies.get("access_token")?.value;
+
   try {
     if (token) {
-      verifyJWT(token);
+      await verifyJWT(token);
     } else {
       throw new UnauthorisedError("Token was not supplied.");
     }
   } catch (e) {
     if (e instanceof UnauthorisedError) {
+      if (request.nextUrl.pathname.startsWith("/api/")) {
+        return NextResponse.json(
+          { error: "Unauthorised transaction." },
+          { status: 401 }
+        );
+      }
       return NextResponse.redirect(new URL("/login", request.url));
     } else {
       console.log("Some other error");
     }
   }
-
-  return NextResponse.next();
 }
 
 export { middleware };
